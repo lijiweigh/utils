bcf241741e0a6bad5b569f72fca19489dcdabaac
jiwei.li <jiwei.li@ly.com>
2019-09-24 20:11:08 +0800

    articles

---
+++ node/fetch-github/test.js
@@ -0,0 +1,53 @@
+const cheerio = require('cheerio')
+const superagent = require('superagent')
+const fs = require("fs")
+const h2m = require("h2m")
+const path = require("path")
+
+let bodyElement = ".markdown-body"
+
+superagent.get("https://github.com/ljianshu/Blog").then(res => {
+
+    let $ = cheerio.load(res.text, {decodeEntities: false})
+    
+    let h3 = $(".markdown-body h3")
+    h3.each((index, item) => {
+        // console.log("--------------")
+        let id = $(item).find("a").eq(0).attr("id")
+        let dirName = id.slice(id.lastIndexOf("-") + 1)
+        
+        // console.log(dirName)
+        let h4 = $(item).nextUntil("h3", "h4")
+        if(h4.length > 0) {
+            fs.mkdirSync(path.resolve(__dirname, "../../articles", dirName), { recursive: true })
+            let promises = []
+            h4.each((index2, item2) => {
+                
+                let fileName = $(item2).find("a").eq(1).html()
+                fileName = fileName.replace(/\s/g,"")
+                fileName = fileName.replace(/\//g,"-")
+                let url = $(item2).find("a").eq(1).attr("href")
+                // console.log(fileName, url)
+                let p = superagent.get(url).then(res => {
+                    let content = cheerio.load(res.text, {decodeEntities: false})
+                    let html = content(bodyElement)
+                    let md = h2m(html)
+                    let w = fs.createWriteStream(path.resolve(__dirname, "../../articles", dirName, fileName + ".md"))
+                    w.write(md,"UTF8")
+                    w.end()
+                }).catch(err => {
+                    console.log(err)
+                })
+                promises.push(p)
+            })
+            Promise.all(promises).then(res => {
+                console.log(`${dirName}成功`)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+        
+    })
+})
+
+
