dd337dcd3a351b5f9eaca60f87af5fea5b812a3b
jiwei.li <jiwei.li@ly.com>
2020-04-09 19:02:44 +0800

    图片旋转,裁剪,压缩

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import VConsole from "vconsole"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
+
+// new VConsole()
 
 new Vue({
   router,
--- src/views/uploadImgHandle/uploadImgHandle.vue
+++ src/views/uploadImgHandle/uploadImgHandle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="uploadImgHandle">
-        <input type="file" accept="image/*" @change="handleInput"/>
+        <input type="file" ref="file" accept="image/*" @change="handleInput"/>
         <div>
             <p>原始图片</p>
             <img :src="origin" alt="">
@@ -37,6 +37,9 @@ export default {
             minimize: "",
         }
     },
+    created() {
+        
+    },
     methods: {
         handleInput(e) {
             let file = e.target.files[0]
@@ -45,25 +48,165 @@ export default {
                 let data64 = e.target.result
                 this.origin = data64
                 let image = new Image()
-                image.onload = () => {
-                    // 图片旋转
-                    this.rotateImg(image)
+                let canvas = document.createElement("canvas")
+                image.onload = async () => {
+                    // 旋转
+                    this.rotate = await this.rotateImg(image, canvas)
+                    let image2 = new Image()
+                    image2.onload = async () => {
+                        // 裁剪
+                        this.cut = await this.cutImg(image2, canvas, 0.75)
+                        let image3 = new Image()
+                        image3.onload = async () => {
+                            // 压缩
+                            this.minimize = await this.minimizeImg(image3, canvas, 500)
+                        }
+                        image3.src = this.cut
+                    }
+                    image2.src = this.rotate
                 }
                 image.src = data64
             }
             fr.readAsDataURL(file)
+            e.target.value = ""
         },
-        rotateImg(image) {
-            EXIF.getData(image, function() {
-                let Orientation = EXIF.getTag(this, "Orientation")
-                console.log(Orientation)
-                console.log(EXIF.getAllTags(this))
-                let canvas = document.createElement("canvas")
-                let ctx = canvas.getContext("2d")
-                canvas.width = image.width
-                canvas.height = image.height
-                ctx.drawImage(image, 0, 0)
-            });
+        cutImg(image, canvas, percent) {
+            return new Promise((resolve, reject) => {
+                try {
+                    let originWidth = image.width
+                    let originHeight = image.height
+                    let originPercent = originWidth / originHeight
+                    let width, height, startX, startY
+                    if(originWidth > originHeight) {
+                        startY = 0
+                        height = originHeight
+                        let midX = Math.round(originWidth / 2)
+                        width = height * percent
+                        startX = midX - Math.round(width / 2)
+                        if(startX < 0) {
+                            startX = 0
+                        }
+                        if(width > originWidth) {
+                            width = originWidth
+                        }
+                    } else {
+                        startX = 0
+                        width = originWidth
+                        let midY = Math.round(originHeight / 2)
+                        height = width / percent
+                        startY = midY - Math.round(height / 2)
+                        if(startY < 0) {
+                            startY = 0
+                        } 
+                        if(height > originHeight) {
+                            height = originHeight
+                        }
+                    }
+                    console.log(startX, startY, width, height)
+                    canvas.width = width
+                    canvas.height = height
+                    let ctx = canvas.getContext("2d")
+                    ctx.fillStyle = "#fff"
+                    ctx.fillRect(0, 0, width, height)
+                    ctx.drawImage(image, startX, startY, width, height, 0, 0, width, height)
+                    resolve(canvas.toDataURL("image/jpeg"))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+
+        },
+        rotateImg(image, canvas) {
+            let _this = this
+            let width = image.width
+            let height = image.height
+            return new Promise((resolve, reject) => {
+                try {
+                    EXIF.getData(image, function() {
+                        let Orientation = EXIF.getTag(this, "Orientation")
+                        console.log("Orientation " + Orientation)
+                        if(Orientation) {
+                            let ctx = canvas.getContext("2d")
+                            ctx.fillStyle = "#fff"
+                            switch(Orientation) {
+                                case 1: 
+                                    canvas.width = width
+                                    canvas.height = height
+                                    ctx.fillRect(0, 0, width, height)
+                                    ctx.drawImage(image, 0, 0)
+                                    break;
+                                case 3: 
+                                    canvas.width = width
+                                    canvas.height = height
+                                    ctx.rotate(180 * Math.PI / 180);
+                                    ctx.fillRect(-width, -height, width, height)
+                                    ctx.drawImage(image, -width, -height)
+                                    break;
+                                case 6: 
+                                    canvas.width = height
+                                    canvas.height = width
+                                    ctx.rotate(90 * Math.PI / 180);
+                                    ctx.fillRect(0, -height, height, width)
+                                    ctx.drawImage(image, 0, -height)
+                                    break;
+                                case 8: 
+                                    canvas.width = height
+                                    canvas.height = width
+                                    ctx.rotate(270 * Math.PI / 180);
+                                    ctx.fillRect(-width, 0, height, width)
+                                    ctx.drawImage(image, -width, 0)
+                                    break;
+                            }
+                            resolve(canvas.toDataURL("image/jpeg"))
+                        } else {
+                            canvas.width = width
+                            canvas.height = height
+                            let ctx = canvas.getContext("2d")
+                            ctx.fillStyle = "#fff"
+                            ctx.fillRect(0, 0, width, height)
+                            ctx.drawImage(image, 0, 0)
+                            resolve(canvas.toDataURL("image/jpeg"))
+                        }  
+                    });
+                } catch(e) {
+                    reject(e)
+                }
+            })
+            
+        },
+        minimizeImg(image, canvas, maxWidth) {
+            return new Promise((resolve, reject) => {
+                try {
+                    let width = image.width
+                    let height = image.height
+                    console.log("origin size: " + width + " * " + height)
+                    if(width <= maxWidth) {
+                        canvas.width = width
+                        canvas.height = height
+                        let ctx = canvas.getContext("2d")
+                        ctx.fillStyle = "#fff"
+                        ctx.fillRect(0, 0, width, height)
+                        ctx.drawImage(image, 0, 0)
+                        let base64 = canvas.toDataURL("image/jpeg")
+                        resolve(base64)
+                        return base64
+                    }
+                    let maxHeight = Math.round(maxWidth / width * height)
+                    canvas.width = maxWidth
+                    canvas.height = maxHeight
+                    let ctx = canvas.getContext("2d")
+                    ctx.fillStyle = "#fff"
+                    ctx.fillRect(0, 0, maxWidth, maxHeight)
+                    ctx.drawImage(image, 0, 0, maxWidth, maxHeight)
+                    let minimize = canvas.toDataURL("image/jpeg")
+                    console.log("minimized size: " + maxWidth + " * " + maxHeight)
+                    resolve(minimize)
+                    return minimize
+                } catch(e) {
+                    reject(e)
+                }
+            })
+            
         }
     }
 }
@@ -73,6 +216,7 @@ export default {
 .uploadImgHandle {
     img {
         width: 500px;
+        border: 2px solid orangered;
     }
 }
 </style>
