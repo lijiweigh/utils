8f2a5a2227b40b0a80c3055c3d5a181f705dc256
lijiwei <1101407798@qq.com>
2020-04-16 22:37:45 +0800

    浏览器事件循环

--- src/views/test3/test3.vue
+++ src/views/test3/test3.vue
@@ -12,7 +12,36 @@ import aCheckboxGroup from "@/components/customize/a-checkbox-group"
 export default {
     name: "test2",
     mounted() {
-        alert(getComputedStyle(this.$refs.stick).position)
+        /**
+         * 执行顺序：
+         *  1.同步代码
+         *  2.promise
+         *  3.requestAnimationFrame
+         *  4.必要的话渲染ui
+         *  5.执行异步代码
+         * 
+         */
+        // alert(getComputedStyle(this.$refs.stick).position)
+        let div = document.createElement("div")
+        div.style.height = "0.5px"
+        div.style.background = "red"
+        this.$refs.stick.appendChild(div)
+        console.log(div.offsetHeight)
+        requestAnimationFrame(() => {
+            alert("requestAnimationFrame")
+            console.log("requestAnimationFrame")
+        })
+        let d = new Date().getTime()
+        Promise.resolve().then(() => {
+            while(new Date().getTime() - d < 5000) {
+                // console.log(new Date().getTime() - d)
+            }
+        })
+        setTimeout(() => {
+            alert("5s")
+            console.log("5s")
+            
+        }, 1000);
     }
    
 }
