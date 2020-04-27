1c25b4f054ea63451802dc9a8c78ae8189c691a4
jiwei.li <jiwei.li@ly.com>
2020-04-27 19:05:31 +0800

    ..

--- utils/test/1.js
+++ utils/test/1.js
@@ -27,24 +27,109 @@ let code = [
 		schema: {
 			type: "string",
 		},
+    },
+    {
+		id: 2,
+		name: "count",
+		in: "query",
+		description: "username",
+		schema: {
+			type: "number",
+		},
+    },
+    {
+		id: 2,
+		name: "list",
+		in: "query",
+		description: "username",
+		schema: {
+			type: "array",
+        },
+        children: [
+			{
+				id: 8,
+				name: "username",
+				in: "query",
+				description: "username",
+				schema: {
+					type: "object",
+                },
+                children: [
+                    {
+                        id: 8,
+                        name: "username",
+                        in: "query",
+                        description: "username",
+                        schema: {
+                            type: "string",
+                        },
+                    },
+                ],
+            },
+            {
+				id: 8,
+				name: "username",
+				in: "query",
+				description: "username",
+				schema: {
+					type: "object",
+                },
+                children: [
+                    {
+                        id: 8,
+                        name: "username",
+                        in: "query",
+                        description: "username",
+                        schema: {
+                            type: "string",
+                        },
+                    },
+                ],
+            },
+            {
+				id: 8,
+				name: "username",
+				in: "query",
+				description: "username",
+				schema: {
+					type: "number",
+                },
+                
+			},
+		],
 	},
 ]
 
-let result = "{"
+let result = "{\n"
 
-code.forEach(item => {
-    result += `"${item.name}": ${getValueByType(item.type)},\n`
-})
+result += setCode(code)
 
 result += "}"
 
+console.log(result)
+
+function setCode(code, type) {
+    let r = ""
+    code.forEach(item => {
+        if(type !== "array") {
+            r += `"${item.name}": `
+        }
+        r += `${getValueByType(item.schema.type)},\n`
+        if(item.children && item.children.length > 0) {
+            r = r.slice(0, -3) + "\n" + setCode(item.children, item.schema.type) + r.slice(-3)
+        }
+    })
+    return r
+}
+
 function getValueByType(type) {
     switch(type) {
-        case "string": return "";
-        case "number": return "0";
-        case "integer": return "0";
-        case "boolean": return "true";
+        case "string": return "\"\"";
+        case "number": return 0;
+        case "integer": return 0;
+        case "boolean": return true;
         case "array": return "[]";
+        case "user": 
         case "object": return "{}";
     }
 }
