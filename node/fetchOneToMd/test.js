209ba8122c0134ac3e5de8f8e8ed26c4f6eee00f
jiwei.li <jiwei.li@ly.com>
2020-05-21 20:03:08 +0800

    ...

--- node/fetchOneToMd/test.js
+++ node/fetchOneToMd/test.js
@@ -4,24 +4,30 @@ const fs = require("fs")
 const h2m = require("h2m")
 const path = require("path")
 
-let URL = "https://mp.weixin.qq.com/s/WbtLO0ji-i9q3JLdkxQ2DQ" 
-let FILENAME = "如何处理浏览器的断网情况？"
+let URL = "https://segmentfault.com/a/1190000020714686" 
+let FILENAME = "HTTP2和HTTP3"
 // 思否的文章打开这个
-// let ELEMENT_SELECTOR = ".card-body"
+let ELEMENT_SELECTOR = ".card-body"
 // 微信公众号的文章打开这个
-let ELEMENT_SELECTOR = "#page-content"
+// let ELEMENT_SELECTOR = "#page-content"
 
 let options = {
     overides: {
         // 思否的文章打开这个
-        // img: function(node) {
-        //     return `![image](https://segmentfault.com/${node.attrs["data-src"]})`
-        // },
+        img: function(node) {
+            let result = ``
+            if(node.attrs["data-src"] && node.attrs["data-src"].startsWith("http")) {
+                result = `![image](${node.attrs["data-src"]})`
+            } else {
+                result = `![image](https://segmentfault.com${node.attrs["data-src"]})`
+            }
+            return result
+        },
 
         // 微信公众号的文章打开这个
-        img: function(node) {
-            return `![image](${node.attrs["data-src"]})`
-        },
+        // img: function(node) {
+        //     return `![image](${node.attrs["data-src"]})`
+        // },
     }
 }
 
@@ -31,7 +37,7 @@ superagent.get(URL).then(res => {
     console.log("----------------------开始解析数据----------------------")
 
     // 微信公众号的文章打开这个
-    res.text = res.text.replace(/ol/g, "code").replace(/li/g, "span")
+    // res.text = res.text.replace(/ol/g, "code").replace(/li/g, "span")
 
     let $ = cheerio.load(res.text, {decodeEntities: false})
     let html = $(ELEMENT_SELECTOR)
